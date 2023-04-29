722e0086fd5735a36d011936e0a6aa2c91e58448
Assume-Zhan <assume0701@gmail.com>
2023-04-29 21:51:00 +0800

    ADD : google sign in

--- pages/Homepage.js
+++ pages/Homepage.js
@@ -2,10 +2,15 @@ import React from "react";
 import { Grid } from "semantic-ui-react";
 import Login from "./Login";
 import Signup from "./Signup";
+import { useNavigate } from "react-router-dom";
+import firebase from "../utils/config";
+import "firebase/compat/auth";
+import 'firebase/compat/database'
 
 function Homepage() {
 
     const [activeItem, setActiveItem] = React.useState("signup");
+    const navigate = useNavigate();
 
     function pressLogin(e){
         e.preventDefault();
@@ -15,6 +20,61 @@ function Homepage() {
     function pressSignup(e){
         e.preventDefault();
         setActiveItem("signup");
+    }
+
+    function pressGoogle(e){
+        e.preventDefault();
+
+        var Provider = new firebase.auth.GoogleAuthProvider();
+        
+        firebase.auth().signInWithPopup(Provider)
+        .then((userCredential) => {
+                var user = userCredential.user;
+                
+                /*
+                 * Push user profile
+                 */
+                var users = firebase.database().ref('com_list/users/' + user.uid);
+                var userLength = firebase.database().ref('com_list/userlength');
+                userLength.once('value', (snapshot) => {
+
+                    /*
+                     * Set and get default image
+                     */
+
+                    if(snapshot.val() == null) {
+                        userLength.set(0);
+                        users.set({
+                            email: user.email,
+                            id: 0,
+                            name: user.email,
+                            imgURL: user.photoURL,
+                            userid: user.uid,
+                        });
+                    }
+                    else{
+                        users.set({
+                            email: user.email,
+                            id: snapshot.val(),
+                            name: user.email,
+                            imgURL: user.photoURL,
+                            userid: user.uid,
+                        });
+                    }
+                    
+                    /* Update user length  */
+                    userLength.set(snapshot.val() + 1);
+
+                    /* Navigate to chatroom */
+                    navigate('/chatroom')
+                })
+            }
+        )
+        .catch(function(error) {
+                create_alert("error", error.message);
+            }
+        )
+
     }
 
     return <Grid style={{height: '100vh', textAlign: "center"}}>
@@ -53,7 +113,7 @@ function Homepage() {
                     </div>
                 </button>
             </form>
-            <form autoComplete='off' className='form' onSubmit={(e) => pressLogin(e)}>
+            <form autoComplete='off' className='form' onSubmit={(e) => pressGoogle(e)}>
                 <button className='form btn block-cube block-cube-hover'>
                     <div className='bg-top'>
                     <div className='bg-inner'></div>
